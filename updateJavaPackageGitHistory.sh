a75ffea47a4e964a9824bb5fe819ac4d56c4727d
Michał Grabarczyk <michal.grabarczyk@gmail.com>
2017-01-11 11:12:04 +0100

    # Perform same replace in commit messages

--- updateJavaPackageGitHistory.sh
+++ updateJavaPackageGitHistory.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Rename in git history all old java package names (old.oldpackage) with new ones (new.newpackage).
-# TODO: Perform same replace in commit messages.
 
 git filter-branch --tree-filter " \
 	find . -type f -name '*.java' -exec sed -i -e 's/old.oldpackage/new.newpackage/g' {} \; && \
 	find . -type d -name oldpackage -execdir mv {} newpackage \; -prune && \
 	find . -type d -name old -execdir mv {} new \; -prune \
-"
+	" \
+	--msg-filter "sed -e 's/old.oldpackage/new.newpackage/g'"
 
 # -execdir will execute the command in each subdirectory
 # -prune causes find to not descend into the current file
